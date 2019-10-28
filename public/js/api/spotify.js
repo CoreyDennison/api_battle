b6495e39382bda077782f8dab0ab9f0b5dace302
Corey Dennison <cmgdennison@gmail.com>
2019-10-28 11:57:03 +0000

    Establishing how to access Spotify API

--- public/js/api/spotify.js
+++ public/js/api/spotify.js
@@ -6,6 +6,16 @@ const getUserData = (accessToken) => {
   }).then(response => response.json())
 };
 
+const getArtist = (accessToken, q) => {
+  return fetch(`https://api.spotify.com/v1/search?q=${q}&type=artist&limit=1`, {
+    headers: {
+      'Authorization': `Bearer ${accessToken}`
+    }
+  }).then(repsonse => response.json())
+  .then(response => console.log(response))
+};
+
 export default {
-  getUserData
+  getUserData,
+  getArtist
 }
--- public/js/pages/battle.js
+++ public/js/pages/battle.js
@@ -1,9 +1,23 @@
-/* import SpotifyAPI from "../api/spotify.js";
-import {getHashParams} from "../helpers/url.js";
-import {STATE_KEY} from "../helpers/constants.js"; */
+//import SpotifyAPI from "../api/spotify.js";
+//import {getHashParams} from "../helpers/url.js";
+//import {STATE_KEY} from "../helpers/constants.js";
+
+let spotifyURL = "https://api.spotify.com/v1/";
 
 function battleFunc(){
-  document.getElementById("art1_name").innerHTML = artist1.value;
-  document.getElementById("art2_name").innerHTML = artist2.value;
+  let artist1Name = artist1.value;
+  let artist2Name = artist2.value;
+
+  const {access_token, state} = getHashParams();
+
+  let artist1Arr = [];
+
+  //review ajax and https://developer.spotify.com/console/get-search-item/
+
+  fetch(spotifyURL + "search?query=" + artist1Name)
+  .then(res => res.json())
+  .then(data => console.log(data));
+
+
 
 }
